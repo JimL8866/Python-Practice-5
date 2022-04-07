b555cde079f88f5183e175bb2972a3af0ec66d14
Jim <chengxuyuan9527@gmail.com>
2022-04-08 07:42:03 +1000

    more practice

--- main.py
+++ main.py
@@ -10,19 +10,120 @@
 #
 # round(84.1, 3)
 
-name = "Jim"
-age = 35
+# name = "Jim"
+# age = 35
+#
+# # 传入参数
+# print("my name is {} and age is {}".format(name, age))
+# print("my name is {0} and age is {1}".format(name, age))
+# print("my name is {name} and age is {age}".format(name="jim", age=35))
+#
+# #f ， 转换为浮点型（默认小数点后保留6位）表示，然后格式化；
+# tpl = "i am {:s}, age {:d}, money {:f}".format("seven", 18, 88888.1)
+# print(tpl)
+#
+# #f ， 转换为浮点型（默认小数点后保留6位）表示，然后格式化；
+# tpl = "i am {:s}, age {:d}, money {:.2f}".format("seven", 18, 88888.1)
+# print(tpl)
 
-# 传入参数
-print("my name is {} and age is {}".format(name, age))
-print("my name is {0} and age is {1}".format(name, age))
-print("my name is {name} and age is {age}".format(name="jim", age=35))
+# list_1 = ['jiu', 'zhangyu', 'luosifen']
+#
+#
+# list_2 = []
+# for item in list_1:
+# 	list_item = list(item)
+# 	print(list_item)
+# 	if list_item[0] in ["j", "z", "d", "f", "h"]:
+# 		list_2.append(item)
+#
+# a = "jiu"
+# a.split()
+# print(a)
 
-#f ， 转换为浮点型（默认小数点后保留6位）表示，然后格式化；
-tpl = "i am {:s}, age {:d}, money {:f}".format("seven", 18, 88888.1)
-print(tpl)
+# def print_avg(*args, **kwargs) -> str:
+#     # Please write your code here
+#     print(kwargs)
+#     print(args)
+#
+# print_avg(1, 2, 3, name="Jim")
+# from statistics import mean
+#
+# list1 = [1, 2, 3, 4, 5]
+# print(mean(list1))
+# for i in range(100, 201):
+# 	if i % 2 !=0 and i % 3 !=0:
+# 		print(i)
+#
+# for i in range(2, 101):
+#     is_prime = True
+#     for x in range(2, i):
+#         if i % x == 0:
+#             is_prime = False
+#     if is_prime:
+#         print(i)
+#
+#
+# def is_prime(m):
+#     prime = True
+#     for i in range(2, m):
+#         if m % i == 0:
+#             prime = False
+#     if prime:
+#         return m
+#     else:
+#         return "not prime"
+#
+#
+# print(is_prime(75))
 
-#f ， 转换为浮点型（默认小数点后保留6位）表示，然后格式化；
-tpl = "i am {:s}, age {:d}, money {:.2f}".format("seven", 18, 88888.1)
-print(tpl)
+my_list = [5,4,3]
+# for i in my_list:
+# 	for n in my_list[1:]:
+# 		for m in my_list[2:]:
+# 			if i **2 + n **2 == m **2:
+# 				print("True")
+#
+
+#
+# def input_list(my_list):
+# 	my_list.sort()
+# 	for i in my_list:
+# 		for n in my_list[1:]:
+# 			for m in my_list[2:]:
+# 				if i ** 2 + n ** 2 == m ** 2:
+# 					return True
+# 	return False
+#
+# print(input_list(my_list))
+# def input_number(num):
+# 	for i in range(0, num + 1 ):
+# 		if i % 3 == 0 and i % 5 ==0:
+# 			print("FizzBuzz")
+# 		elif i % 3 == 0:
+# 			print("Fizz")
+# 		elif i % 5 == 0:
+# 			print("Buzz")
+# 		else:
+# 			print(i)
+#
+# input_number(15)
+#
+# print(0%3)
+
+# def rotate(l, k):
+#   for i in range(k):
+#     l.insert(0, l.pop())
+#   r
+#   eturn l
+#
+# # print(rotate([1,2,3],1))
+# list1=["a", "b", "c"]
+# list1.pop()
+# print(list1)
+#
+# list3=[0,1,4,]
+
+
+# sqs = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(sqs[::-1])
 
